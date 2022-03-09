1bc866aaff08362058f1b2d629bc0ede7b4c91e2
Vadim Zarifullin <65528175+Zarvad23@users.noreply.github.com>
2022-03-09 09:16:56 +0500

    Работает

--- view/js/app.js
+++ view/js/app.js
@@ -1,66 +1,42 @@
-let SR_API_ANIMES =
-  "https://service.sovetromantica.com/v1/animesearch?anime_name=";
-let SHIKI_API_IMAGES = "https://shikimori.one/api/animes/";
-let SR_ONGOINGS = "https://service.sovetromantica.com/v1/ongoing";
-let SR_AnimeId = "https://service.sovetromantica.com/v1/anime/";
-let vadimchik = document.querySelector(".player_overlay");
+let ShikiTopThirty = "https://shikimori.one/api/animes?limit=32&order=ranked";
+let ShikiSearchResults = 'https://shikimori.one/api/animes?limit=50&order=ranked&search=';
+let playerOverlay = document.querySelector(".player_overlay");
 
-getOngoings(SR_ONGOINGS);
+getTopThirty(ShikiTopThirty);
 
-async function getOngoings(url) {
-  let resp = await fetch(url);
-  let Ids = await resp.json();
-  for (id of Ids) {
-    let resp = await fetch(SR_AnimeId + id);
-    let data = await resp.json();
-    showMovies(data, false);
-  }
+async function getTopThirty(url) {
+    let res = await fetch(url)
+    let data = await res.json();
+    showMovies(data)
 }
 
-async function getMovies(url) {
-  let resp = await fetch(url);
-  let respData = await resp.json();
-  for (id of respData) {
-    let resp = await fetch(SR_AnimeId + id.anime_id);
-    let data = await resp.json();
-    showMovies(data, false);
-  }
+async function searchResults(url) {
+    let res = await fetch(url)
+    let data = await res.json();
+    showMovies(data)
 }
 
-async function getImage(id) {
-  let resp = await fetch(SHIKI_API_IMAGES + id);
-  let respData = await resp.json();
-  return respData.image.original;
-}
-
-function showMovies(data, isClearable = true) {
+function showMovies(data) {
   let moviesEl = document.querySelector(".movies");
-  if (isClearable) {
-    document.querySelector(".movies").innerHTML = "";
-  }
-  for (movie of data) {
-    if (movie.anime_shikimori == 0) {
-      continue;
-    }
-    (async () => {
-      let movieEl = document.createElement("div");
-      let AnimeImage = await getImage(movie.anime_shikimori);
-      let title = await movie.anime_name_russian;
-      movieEl.classList.add("movie");
-      movieEl.innerHTML = `
+  document.querySelector(".movies").innerHTML = "";
+  for (let i = 0; i <= data.length -1 ;i++) {
+      let anime = data[i];
+        let movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
         <div class="movie__cover-inner">
         <img
-          src="https://shikimori.one/${AnimeImage}"
+          src="https://shikimori.one/${anime.image.original}"
           class="movie__cover"
-          alt="${movie.nameRu}" />
+          alt="${anime.russian}" />
         <div class="movie__cover--darkened"></div>
       </div>
       <div class="movie__info">
-        <div class="movie__title">${title}</div>
+        <div class="movie__title">${anime.russian}</div>
       </div>
         `;
-      movieEl.dataset.id = `${movie.anime_shikimori}`;
-      await movieEl.addEventListener("click", async function () {
+      movieEl.dataset.id = `${anime.id}`;
+      movieEl.addEventListener("click", async function () {
         let res = await fetch(
           `https://metamedia.glitch.me/api/search?shikimori_id=${movieEl.dataset.id}&sign=3052d19319b8daf0a194c9a770fc9a3d80bfe1bd3e0e28b7cfd7aacebaaf4f6b`
         );
@@ -73,28 +49,23 @@ function showMovies(data, isClearable = true) {
         player.src = data.results[0].link;
       });
       moviesEl.appendChild(movieEl);
-    })();
-  }
+    };
 }
 
 let form = document.querySelector("form");
 let search = document.querySelector(".header__search");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  document.querySelector(".movies").innerHTML = "";
-  let Anime_Search_Encoded = encodeURIComponent(search.value);
-
-  let apiSearchUrl = `${SR_API_ANIMES}${Anime_Search_Encoded}`;
-  if (search.value) {
-    getMovies(apiSearchUrl);
-
+    e.preventDefault();
+    let Anime_Search_Encoded = encodeURIComponent(search.value);
+    let apiSearchUrl = `${ShikiSearchResults}${Anime_Search_Encoded}`;
+    console.log(apiSearchUrl)
+    searchResults(apiSearchUrl);
     search.value = "";
-  }
 });
 
-vadimchik.addEventListener("click", function () {
+playerOverlay.addEventListener("click", function () {
   let andrey = document.querySelector("#player");
   andrey.classList.add("hide");
-  vadimchik.classList.add("hide");
+  playerOverlay.classList.add("hide");
 });
